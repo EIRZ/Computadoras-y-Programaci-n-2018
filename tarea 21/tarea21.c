b406cca1d21a2cf97c6fa683bb4435cafc63b488
EIRZ <31521295+EIRZ@users.noreply.github.com>
2017-11-14 14:46:15 -0600

    tarea 21

---
+++ tarea 21/tarea21.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "tarea21.h"
+
+int main(int argc, char *argv[]) {
+	
+	fnLetraE ();
+	fnLetraD ();
+	fnLetraU ();
+	fnLetraA ();
+	fnLetraR ();
+	fnLetraD ();
+	fnLetraO ();
+	
+	return 0;
+}
---
+++ tarea 21/tarea21.h
@@ -0,0 +1,43 @@
+void  fnLetraE () {
+           printf ( " EEEE \n " );
+		   printf ( " E \n " );
+           printf ( " EEE \n " );
+           printf ( " E \n " );
+           printf ( " EEEE \n " );
+}
+void  fnLetraD () {
+           printf ( " DDDD \n " );
+           printf ( " D   D \n " );
+           printf ( " D   D \n " );
+           printf ( " D   D \n " );
+           printf ( " DDDD \n " );
+}
+void  fnLetraU () {
+           printf ( " U   U \n " );
+           printf ( " U   U \n " );
+           printf ( " U   U \n " );
+           printf ( " U   U \n " );
+           printf ( " UUUUU \n " );
+}
+void  fnLetraA () {
+           printf ( " AAAA \n " );
+           printf ( " A  A \n " );
+           printf ( " AAAA \n " );
+           printf ( " A  A \n " );
+           printf ( " A  A \n " );
+}
+void  fnLetraR () {
+           printf ( " RRRR \n " );
+           printf ( " R  R \n " );
+           printf ( " RRRR \n " );
+           printf ( " R   R \n " );
+           printf ( " R    R \n " );
+}
+void  fnLetraO () {
+           printf ( " OOOO \n " );
+           printf ( " O  O \n " );
+           printf ( " O  O \n " );
+           printf ( " O  O \n " );
+           printf ( " OOOO \n " );   
+}
+
